34b0901483d3a30e1acf4a37e7e4e1c75f361196
cessadev <cesarurb.dev@gmail.com>
2024-10-23 11:40:25 -0500

    First commit

---
+++ src/main/java/com/cessadev/demo_apirest_covid19/DemoApirestCovid19Application.java
@@ -0,0 +1,13 @@
+package com.cessadev.demo_apirest_covid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApirestCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApirestCovid19Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cessadev/demo_apirest_covid19/DemoApirestCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cessadev.demo_apirest_covid19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApirestCovid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
